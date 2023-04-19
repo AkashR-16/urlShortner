b9638c6243d474748b5a8932ca0a92921de16494
AkashR-16 <99939852+AkashR-16@users.noreply.github.com>
2023-04-19 21:44:09 +0530

    Delete src directory

--- src/main/java/com/akash/urlshortener/Repo/ShortURLRepo.java
+++
@@ -1,5 +0,0 @@
-package com.akash.urlshortener.Repo;
-import com.akash.urlshortener.Models.ShortURL;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ShortURLRepo extends JpaRepository<ShortURL,Long> {
-}
